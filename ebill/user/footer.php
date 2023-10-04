60bc1275a014154b824881d4172201ee8f485240
Omkar Lingayat <92253155+Omkarlingayat@users.noreply.github.com>
2023-10-04 11:32:55 +0530

    Add files via upload

---
+++ ebill/user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <U><I>Created by </I> <B style="color:teal"> Vaishnavi Trikal</a></B></U>
+            </div>
+        </div>
+    </footer>
